42a769ef64f4236bf6b8c9a8cd49542956307393
Shayan Dasgupta <shayandasgupta1502@gmail.com>
2023-05-23 00:04:29 +0530

    update Q-table

--- main.py
+++ main.py
@@ -1,56 +1,54 @@
 import minimax
 import game
 import random
-import RL
+import RLearning
 
 
-# to check if either algorithm is working properly
-class randomPlayer:
-    def __init__(self, symbol):
-        self.symbol = symbol
-
-    def choose_move(self, board):
-        move = (self.symbol, random.randint(0, 6))
-        while not board.is_valid_move(board, move):
-            move = (self.symbol, random.randint(0, 6))
-
-        return move
-
-
-def main(board, P1, P2):
+def main(board, P1, P2, S1, S2):
     p1_score = 0
     p2_score = 0
 
     for _ in range(10000):
         board.refresh_board(board)
+
         while True:
             move1 = P1.choose_move(board)
+            state1 = ''.join([item for sublist in board.board for item in sublist])
             board.do_move(board, move1)
 
-            if board.is_winner(board, board.RED):
+            if board.is_winner(board, S1):
                 p1_score += 1
-                print("P1 wins")
+                reward = 100
+                state2 = ''.join([item for sublist in board.board for item in sublist])
+                P1.update_q_table(state1, move1[1], state2, reward)
                 break
 
             move2 = P2.choose_move(board)
             board.do_move(board, move2)
 
-            if board.is_winner(board, board.BLACK):
+            if board.is_winner(board, S2):
                 p2_score += 1
-                print("P2 wins")
+                reward = -100
+                state2 = ''.join([item for sublist in board.board for item in sublist])
+                P1.update_q_table(state1, move1[1], state2, reward)
                 break
 
             if board.is_draw(board):
-                print("draw")
+                reward = 0
+                state2 = ''.join([item for sublist in board.board for item in sublist])
+                P1.update_q_table(state1, move1[1], state2, reward)
                 break
 
     print("Total no:of wins for P1 :", p1_score)
     print("Total no: of wins for P2 :", p2_score)
+    return p1_score, p2_score
 
 
 if __name__ == '__main__':
     board = game.ConnectFour()
-    player1 = minimax.MiniMax(15, board.RED)
-    player2 = randomPlayer(board.BLACK)
+    playermini = minimax.MiniMax(3, board.RED)
+    playerRL = RLearning.QLearning(board.BLACK, alpha=0.5)
 
-    main(board, player1, player2)
+    for _ in range(1000):
+        print("iter:", _)
+        main(board, playerRL, playermini, playerRL.symbol, playermini.symbol)
