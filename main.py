abed4e2db8f8f7f645e119967d299c56d6ecb123
Shayan Dasgupta <shayandasgupta1502@gmail.com>
2023-05-23 13:46:06 +0530

    increase episodes

--- main.py
+++ main.py
@@ -70,13 +70,13 @@ if __name__ == '__main__':
 
     # minimax plays
     print("Minimax is playing...")
-    for episode in range(100):
+    for episode in range(1000):
         miniscore, randomscore = main(board, playermini, playerrandom, board.RED, board.BLACK)
         miniscores.append(miniscore)
 
     # untrained RL plays
     print("Untrained RL is playing...")
-    for episode in range(1, 101):
+    for episode in range(1, 1001):
         RL1score, randomscore = main(board, playerRL1, playerrandom, board.RED, board.BLACK)
         RL1scores.append(RL1score)
 
@@ -88,23 +88,23 @@ if __name__ == '__main__':
 
     # other RL plays
     print("Training the other RL player...")
-    for episode in range(1, 1001):
+    for episode in range(1, 5001):
         main(board, playerRL2, playerrandom, board.RED, board.BLACK)
 
         # Changing the learning rate and the exploration rate dynamically
         alpha = max(0.1, 0.8 / episode)
         epsilon = max(0.1, 0.9 / episode)
-        playerRL1.learning_rate = alpha
-        playerRL1.exploration_factor = epsilon
+        playerRL2.learning_rate = alpha
+        playerRL2.exploration_factor = epsilon
 
     print("Trained RL is playing...")
-    for episode in range(1, 101):
+    for episode in range(1, 1001):
         RL2score, randomscore = main(board, playerRL2, playerrandom, board.RED, board.BLACK)
         RL2scores.append(RL2score)
 
-    episodes = [i for i in range(1, 101)]
-    plt.plot(episodes, RL1scores, label='Untrained Q-Learning(alpha=0.9, epsilon=0.8')
-    plt.plot(episodes, RL2scores, label='Trained Q-Learning(alpha=0.9, epsilon=0.8')
+    episodes = [i for i in range(1, 1001)]
+    plt.plot(episodes, RL1scores, label='Untrained Q-Learning')
+    plt.plot(episodes, RL2scores, label='Trained Q-Learning')
     plt.plot(episodes, miniscores, label='Minimax')
     plt.legend()
     plt.show()
