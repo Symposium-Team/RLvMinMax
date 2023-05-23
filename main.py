f95adab38e9f359bf771edb18369d659122d8064
Shayan Dasgupta <shayandasgupta1502@gmail.com>
2023-05-23 12:58:06 +0530

    changed comparison algorithm

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import minimax
 import game
 import RLearning
 import matplotlib.pyplot as plt
+import random
+
+
+class randomPlayer:
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def choose_move(self, board):
+        move = (self.symbol, random.randint(0, 6))
+        while not board.is_valid_move(board, move):
+            move = (self.symbol, random.randint(0, 6))
+
+        return move
 
 
 def main(board, P1, P2, S1, S2):
     p1_score = 0
     p2_score = 0
 
-    for _ in range(50000):
+    for _ in range(5000):
         board.refresh_board(board)
 
         while True:
@@ -20,7 +33,8 @@ def main(board, P1, P2, S1, S2):
                 p1_score += 1
                 reward = 100
                 state2 = ''.join([item for sublist in board.board for item in sublist])
-                P1.update_q_table(state1, move1[1], state2, reward)
+                if isinstance(P1, RLearning.QLearning):
+                    P1.update_q_table(state1, move1[1], state2, reward)
                 break
 
             move2 = P2.choose_move(board)
@@ -30,7 +44,8 @@ def main(board, P1, P2, S1, S2):
                 p2_score += 1
                 reward = -100
                 state2 = ''.join([item for sublist in board.board for item in sublist])
-                P1.update_q_table(state1, move1[1], state2, reward)
+                if isinstance(P1, RLearning.QLearning):
+                    P1.update_q_table(state1, move1[1], state2, reward)
                 break
 
             if board.is_draw(board):
@@ -39,38 +54,57 @@ def main(board, P1, P2, S1, S2):
                 P1.update_q_table(state1, move1[1], state2, reward)
                 break
 
-    print("Total no:of wins for P1 :", p1_score)
-    print("Total no: of wins for P2 :", p2_score)
     return p1_score, p2_score
 
 
 if __name__ == '__main__':
     board = game.ConnectFour()
     playermini = minimax.MiniMax(3, board.RED)
-    playerRL = RLearning.QLearning(board.BLACK, alpha=0.9, epsilon=0.8)
+    playerRL1 = RLearning.QLearning(board.RED, alpha=0.9, epsilon=0.8)
+    playerRL2 = RLearning.QLearning(board.RED, alpha=0.9, epsilon=0.8)
+    playerrandom = randomPlayer(board.BLACK)
 
-    episode = 1
-    RLscores = []
+    RL1scores = []
+    RL2scores = []
     miniscores = []
-    while True:
-        print("Episode:", episode)
-        RLscore, miniscore = main(board, playerRL, playermini, playerRL.symbol, playermini.symbol)
-        RLscores.append(RLscore)
+
+    # minimax plays
+    print("Minimax is playing...")
+    for episode in range(100):
+        miniscore, randomscore = main(board, playermini, playerrandom, board.RED, board.BLACK)
         miniscores.append(miniscore)
 
-        if RLscore > miniscore or episode == 500:
-            break
+    # untrained RL plays
+    print("Untrained RL is playing...")
+    for episode in range(1, 101):
+        RL1score, randomscore = main(board, playerRL1, playerrandom, board.RED, board.BLACK)
+        RL1scores.append(RL1score)
 
         # Changing the learning rate and the exploration rate dynamically
         alpha = max(0.1, 0.8 / episode)
         epsilon = max(0.1, 0.9 / episode)
-        playerRL.learning_rate = alpha
-        playerRL.exploration_factor = epsilon
+        playerRL1.learning_rate = alpha
+        playerRL1.exploration_factor = epsilon
 
-        episode += 1
+    # other RL plays
+    print("Training the other RL player...")
+    for episode in range(1, 1001):
+        main(board, playerRL2, playerrandom, board.RED, board.BLACK)
 
-    episodes = [i for i in range(1, episode + 1)]
-    plt.plot(episodes, RLscores, label='Q-Learning')
+        # Changing the learning rate and the exploration rate dynamically
+        alpha = max(0.1, 0.8 / episode)
+        epsilon = max(0.1, 0.9 / episode)
+        playerRL1.learning_rate = alpha
+        playerRL1.exploration_factor = epsilon
+
+    print("Trained RL playing...")
+    for episode in range(1, 101):
+        RL2score, randomscore = main(board, playerRL2, playerrandom, board.RED, board.BLACK)
+        RL2scores.append(RL2score)
+
+    episodes = [i for i in range(1, 101)]
+    plt.plot(episodes, RL1scores, label='Untrained Q-Learning(alpha=0.9, epsilon=0.8')
+    plt.plot(episodes, RL2scores, label='Trained Q-Learning(alpha=0.9, epsilon=0.8')
     plt.plot(episodes, miniscores, label='Minimax')
     plt.legend()
     plt.show()
