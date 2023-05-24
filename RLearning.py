f97dabc63fe111312671565461e48da0d8087459
Shayan Dasgupta <shayandasgupta1502@gmail.com>
2023-05-24 11:12:45 +0530

    using Q-network

--- RLearning.py
+++ RLearning.py
@@ -1,52 +1,93 @@
 import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
+
+
+class QNetwork(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, output_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 class QLearning:
     def __init__(self, symbol, alpha=0.1, gamma=0.9, epsilon=0.1):
         self.symbol = symbol
-        self.learning_rate = alpha  # learning rate
-        self.discount_factor = gamma  # discount factor
-        self.exploration_factor = epsilon  # exploration factor
-        self.q_table = {}
-
-    def state_to_string(self, board):
-        rows, cols = len(board), len(board[0])
-        binary_vector = np.zeros((rows, cols))
-
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j] == self.symbol:
-                    binary_vector[i][j] = 1
-                elif board[i][j] != ' ':
-                    binary_vector[i][j] = -1
-
-        return binary_vector.flatten().tostring()
+        self.learning_rate = alpha
+        self.discount_factor = gamma
+        self.exploration_factor = epsilon
+        self.q_network = QNetwork(42, 7)
+        self.optimizer = optim.Adam(self.q_network.parameters(), lr=self.learning_rate)
 
     def choose_move(self, board):
-        state = self.state_to_string(board.board).strip()
-
-        if random.random() < self.exploration_factor or state not in self.q_table:
-            valid_moves = [col for col in range(7) if board.is_valid_move(board, (self.symbol, col))]
-            move = random.choice(valid_moves)
-            return self.symbol, move
-
-        q_values = self.q_table[state]
-        valid_moves = [col for col in range(7) if board.is_valid_move(board, (self.symbol, col))]
-        best_moves = np.argwhere(q_values[valid_moves] == np.amax(q_values[valid_moves])).flatten()
-        move_index = random.choice(best_moves)
-        move = valid_moves[move_index]
-        return self.symbol, move
+        state = self.preprocess_state(board.board)
+        state_tensor = torch.from_numpy(state).float().unsqueeze(0)
+        q_values = self.q_network(state_tensor)
+        action = torch.argmax(q_values, dim=1).item()
+        move = (self.symbol, action)
+        return move
 
     def update_q_table(self, state, action, next_state, reward):
-        if state not in self.q_table:
-            self.q_table[state] = np.zeros(7)
+        state = self.preprocess_state(state)
+        next_state = self.preprocess_state(next_state)
 
-        if next_state not in self.q_table:
-            self.q_table[next_state] = np.zeros(7)
+        state_tensor = torch.from_numpy(state).float().unsqueeze(0)
+        next_state_tensor = torch.from_numpy(next_state).float().unsqueeze(0)
 
-        current_q_value = self.q_table[state][action]
-        max_next_q_value = np.max(self.q_table[next_state])
-        new_q_value = (1 - self.learning_rate) * current_q_value + self.learning_rate * (
-                    reward + self.discount_factor * max_next_q_value)
-        self.q_table[state][action] = new_q_value
+        q_values = self.q_network(state_tensor)
+        next_q_values = self.q_network(next_state_tensor)
+        max_next_q_value = torch.max(next_q_values).item()
+
+        q_values[0][action] = (1 - self.learning_rate) * q_values[0][action] + \
+                              self.learning_rate * (reward + self.discount_factor * max_next_q_value)
+
+        loss = nn.MSELoss()(q_values, self.q_network(next_state_tensor))
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def preprocess_state(self, board):
+        state = np.zeros((6, 7), dtype=np.float32)
+        for i in range(6):
+            for j in range(7):
+                if board[i][j] == self.symbol:
+                    state[i][j] = 1.0
+                elif board[i][j] != ' ':
+                    state[i][j] = -1.0
+        return state.flatten()
+
+    def train(self, board, num_episodes=5000):
+        state_size = len(self.preprocess_state(board.board))
+        action_size = len(board.board[0])
+
+        self.q_network = QNetwork(state_size, action_size)
+        self.optimizer = optim.Adam(self.q_network.parameters(), lr=self.learning_rate)
+
+        for episode in range(num_episodes):
+            board.refresh_board(board)
+            done = False
+
+            while not done:
+                current_state = board.board.copy()
+
+                move = self.choose_move(board)
+                board.do_move(board, move)
+
+                reward = 0
+                if board.is_winner(board, self.symbol):
+                    reward = 100
+                    done = True
+                elif board.is_draw(board):
+                    reward = 0
+                    done = True
+
+                self.update_q_table(current_state, move[1], board.board, reward)
