de870432204aee1c2da6339d4a25e014bf4955c5
Shayan Dasgupta <shayandasgupta1502@gmail.com>
2023-05-24 08:48:55 +0530

    minor adjustment

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ class randomPlayer:
         return move
 
 
-def main(board, P1, P2, S1, S2):
+def main(board, P1, P2, train=False):
     p1_score = 0
     p2_score = 0
 
-    for _ in range(5000):
+    for _ in range(500):
         board.refresh_board(board)
 
         while True:
@@ -29,29 +29,29 @@ def main(board, P1, P2, S1, S2):
             state1 = ''.join([item for sublist in board.board for item in sublist])
             board.do_move(board, move1)
 
-            if board.is_winner(board, S1):
+            if board.is_winner(board, P1.symbol):
                 p1_score += 1
                 reward = 100
                 state2 = ''.join([item for sublist in board.board for item in sublist])
-                if isinstance(P1, RLearning.QLearning):
+                if isinstance(P1, RLearning.QLearning) and train:
                     P1.update_q_table(state1, move1[1], state2, reward)
                 break
 
             move2 = P2.choose_move(board)
             board.do_move(board, move2)
 
-            if board.is_winner(board, S2):
+            if board.is_winner(board, P2.symbol):
                 p2_score += 1
                 reward = -100
                 state2 = ''.join([item for sublist in board.board for item in sublist])
-                if isinstance(P1, RLearning.QLearning):
+                if isinstance(P1, RLearning.QLearning) and train:
                     P1.update_q_table(state1, move1[1], state2, reward)
                 break
 
             if board.is_draw(board):
                 reward = 0
                 state2 = ''.join([item for sublist in board.board for item in sublist])
-                if isinstance(P1, RLearning.QLearning):
+                if isinstance(P1, RLearning.QLearning) and train:
                     P1.update_q_table(state1, move1[1], state2, reward)
                 break
 
@@ -64,19 +64,21 @@ if __name__ == '__main__':
     playerRL = RLearning.QLearning(board.RED, alpha=0.9, epsilon=0.8)
     playerrandom = randomPlayer(board.BLACK)
 
-    RLscores = []
+    RLscores1 = []
+    RLscores2 = []
     miniscores = []
 
     # minimax plays
     print("Minimax is playing...")
-    for episode in range(1000):
-        miniscore, randomscore = main(board, playermini, playerrandom, board.RED, board.BLACK)
+    for episode in range(100):
+        miniscore, randomscore = main(board, playermini, playerrandom)
         miniscores.append(miniscore)
 
     # RL plays
-    print("Training the other RL player...")
-    for episode in range(1, 5001):
-        main(board, playerRL, playerrandom, board.RED, board.BLACK)
+    print("Training the RL player...")
+    for episode in range(1, 501):
+        RLscore, miniscore = main(board, playerRL, playermini, train=True)
+        RLscores1.append(RLscore)
 
         # Changing the learning rate and the exploration rate dynamically
         alpha = max(0.1, 0.8 / episode)
@@ -85,12 +87,13 @@ if __name__ == '__main__':
         playerRL.exploration_factor = epsilon
 
     print("Trained RL is playing...")
-    for episode in range(1, 1001):
-        RLscore, randomscore = main(board, playerRL, playerrandom, board.RED, board.BLACK)
-        RLscores.append(RLscore)
+    for episode in range(1, 101):
+        RLscore, randomscore = main(board, playerRL, playerrandom)
+        RLscores2.append(RLscore)
 
-    episodes = [i for i in range(1, 1001)]
-    plt.plot(episodes, RLscores, label='Trained Q-Learning')
+    episodes = [i for i in range(1, 101)]
+    plt.plot(episodes, RLscores1, label='Mid-training Q-Learning')
+    plt.plot(episodes, RLscores2, label='Trained Q-Learning')
     plt.plot(episodes, miniscores, label='Minimax')
     plt.legend()
     plt.show()
